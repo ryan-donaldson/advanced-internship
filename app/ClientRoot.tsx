43c842475b7f6c0efafcc9d8eb43c26d150e0c22
ryan-donaldson <ryanwblue@gmail.com>
2026-07-27 18:51:15 -0600

    Removed AuthProvider

--- app/ClientRoot.tsx
+++ app/ClientRoot.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import LoginModal from "@/components/LoginModal";
-import AuthProvider from "@/providers/AuthProvider";
 
 export default function ClientRoot({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
+    <>
       {children}
       <LoginModal />
-    </AuthProvider>
+    </>
   );
 }
